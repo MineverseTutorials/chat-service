e0c0d2933badc647936f71e82d53ff4c29a6a898
Mineverse Tutorials <workcrownedstudios@gmail.com>
2025-01-27 14:49:29 -0500

    server.js

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));  // Serve static files from the 'public' directory
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
